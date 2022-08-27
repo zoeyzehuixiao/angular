529894420fa4aa1300666a9dade62997aa2e4c76
zoeyzehuixiao <zoeyxiao98@gmail.com>
2022-08-27 19:50:11 -0400

    moved data logic from controller to model

--- Evaluation/script.js
+++ Evaluation/script.js
@@ -1,180 +1,192 @@
+// -----------------------API------------------------------
 const api = (() => {
     const url = 'http://localhost:4232'
     const coursePath = '/courseList';
     const get = () => {
-        return fetch(url + coursePath).then((response) => {
-            return response.json()
-        })
+        return fetch(url + coursePath)
+                    .then((response) => response.json())
     }
     return {
         get
     }
 })();
 
+// -----------------------View----------------------------
 const View = (() => {
-	const render = (element, tmp) => {
-        element.innerHTML = tmp;
+    //render object contains all functions that triggers rendering
+    const render = {
+        elementHtml: (element, tmp) => {
+            element.innerHTML = tmp;
+        },
+        focus: (element) => {
+            if(element.style.backgroundColor === 'deepskyblue'){
+                element.style.backgroundColor = null
+            }else{   
+                console.log(element)
+                element.style.backgroundColor = 'deepskyblue'
+            }
+        },
+        confirm: (credit) => {
+            return confirm(`You have chosen ${credit} credits for this semester. You cannot change once you submit. Do you want to confirm?`)
+        },
+        disableButton: (element) => {
+            element.disabled = true;
+        },
     }
-    const focus = (element) => {
-        if(element.style.backgroundColor === 'deepskyblue'){
-            element.style.backgroundColor = null
-        }else{   
-            console.log(element)
-            element.style.backgroundColor = 'deepskyblue'
+    //tmp object contains functions that create templates used in render
+    const tmp = {
+        courseTmp: (data) => {
+            let tmp = '';
+            for(const entry of data){
+                tmp += `
+                <li class="course_list_item" id=${entry.courseId}>
+                    <div class="course_name">${entry.courseName}</div>
+                    <div class="course_type">${entry.required == 'true'? 'Compulsory': 'Elective'}</div>
+                    <div class="course_credit"> Course Credit: ${entry.credit}</div>
+                </li>
+                `
+            }
+            return tmp
+        },
+        creditTmp: (credit) => {
+            return `${credit}`
         }
+        
     }
-    const toggleButton = () => {
-        const element = document.querySelector('#submit')
-        element.disabled = true;
-    }
-    const tmp = (data) => { //data is array
-        let tmp = '';
-        for(const entry of data){
-            tmp += `
-            <li class="course_list_item" id=${entry.courseId}>
-                <div class="course_name">${entry.courseName}</div>
-                <div class="course_type">${entry.required == 'true'? 'Compulsory': 'Elective'}</div>
-                <div class="course_credit"> Course Credit: ${entry.credit}</div>
-            </li>
-            `
-        }
-        return tmp
-    }
-    const creditTmp = (credit) => {
-        return `${credit}`
-    }
+    //selector strings 
 	const selectorContainer = {
         credit: '#credit',
         submit: '#submit',
-
         courseList: '.course_list',
         selectedList: '.selected_list'
     }
-	return{render, focus, toggleButton, tmp, creditTmp, selectorContainer}
+	return{render, tmp, selectorContainer}
 })();
 
+// --------------------------Model----------------------------
 const Model = ((api, view) => {
-    
+    const {get} = api;
 	class State{
-        #courseState = []; //courses listing
-        #focusState = []; //courses being focused
-        #credit = 0;
-        #submitState = []; //courses selected and submitted
-
+        //private properties for storing data
+        #courseState = []; //courses listing [{}, {}, ...]
+        #focusState = []; //courses being focused [id1, id2, ...]
+        #submitState = []; //courses selected and submitted [{}, {}, ...]
+        #credit = 0; //number of current credit
+        //getters
         get courseState(){
             return this.#courseState;
-        } 
-        set courseState(courses){
-            this.#courseState = courses
-            const tmp = view.tmp(this.#courseState);
-            const element = document.querySelector(view.selectorContainer.courseList)
-            view.render(element, tmp)
         }
-
         get focusState(){
             return this.#focusState;
         }
-        set focusState(focus){
-            console.log(+focus.id)
-            if(this.#focusState.includes(+focus.id)){
-                this.#focusState = [...this.#focusState.filter((id)=> id !== +focus.id)]
-            }else{
-                this.#focusState = [...this.#focusState, +focus.id];
-            }
-            view.focus(focus)
-        }
-
         get credit(){
             return this.#credit
-        }
-        set credit(credit){
-            this.#credit = credit
-            const element = document.querySelector(view.selectorContainer.credit)
-            const tmp = view.creditTmp(this.#credit)
-            view.render(element, tmp)
         }
         get submitState(){
             return this.#submitState
         }
-        set submitState(submitted){
-            this.#submitState = submitted
-
-            const tmp = view.tmp(this.#submitState);
-            const element = document.querySelector(view.selectorContainer.selectedList)
-            view.render(element, tmp)
-            view.toggleButton()
+        //setters 
+        set courseState(courses){
+            this.#courseState = courses
+            //update view
+            const tmp = view.tmp.courseTmp(this.#courseState);
+            const element = document.querySelector(view.selectorContainer.courseList)
+            view.render.elementHtml(element, tmp)
         }
-
+        set focusState(focusing){
+            this.#focusState = focusing;
+        }
+        set credit(credit){
+            this.#credit = credit
+            //update view
+            const element = document.querySelector(view.selectorContainer.credit)
+            const tmp = view.tmp.creditTmp(this.#credit)
+            view.render.elementHtml(element, tmp)
+        }
+        set submitState(submitted){
+            this.#submitState = submitted;
+            //update view
+            const tmp = view.tmp.courseTmp(this.submitState);
+            const element = document.querySelector(view.selectorContainer.selectedList)
+            view.render.elementHtml(element, tmp)
+            const button = document.querySelector('#submit')
+            view.render.disableButton(button)
+        }
+        //functions to perform data logic
+        updateFocus = (target)=>{
+            const [course] = this.courseState.filter((obj) => {
+                return (obj.courseId === +target.id)
+            })
+            if(!this.#focusState.includes(+target.id)){
+                if((course.credit + this.credit) > 18){
+                    alert("You can only choose up to 18 credits in one semester")
+                }else{
+                    this.credit += course.credit
+                    this.#focusState = [...this.focusState, +target.id];
+                    view.render.focus(target)
+                }
+            }else{
+                this.credit -= course.credit
+                this.#focusState = [...this.focusState.filter((num)=> num !== +target.id)]
+                view.render.focus(target)
+            }
+        }
+        attemptSubmit = (data) => {
+            if(view.render.confirm(this.credit)){
+                this.submitState = this.courseState.filter((course)=>{
+                    return data.includes(course.courseId)
+                })
+                this.courseState = this.courseState.filter((course)=>{
+                    return !data.includes(course.courseId)
+                })
+                
+            }   
+        }
 	}
-    const {get, getCourse} = api;
 
 	return {
         get,
-        getCourse,
         State,
     }
 })(api, View);
 
-
-
-
-
+// --------------------------Controller----------------------------
 const Controller = ((model, view) => {
-    const state = new model.State(); //instance of state
+    const state = new model.State(); 
+
 	const init = () => {
         model.get().then((data)=>{
             state.courseState = data
             state.credit = 0
         })
     }
+
     const focusCourse = () => {
         const element = document.querySelector(view.selectorContainer.courseList)
         element.addEventListener('click', eventHandler)
-        let target = undefined;
+        
         function eventHandler(event){
+            let target = null;
             if(event.target.className === "course_list_item"){
                 target = event.target;
             }else{
                 target = event.target.parentElement;
             }
-            
-            
-            if(!state.focusState.includes(state.courseState[target.id - 1].courseId)){
-                console.log(state.courseState[target.id - 1].credit + state.credit)
-                if((state.courseState[target.id - 1].credit + state.credit) > 18){
-                    alert("You can only choose up to 18 credits in one semester")
-                }else{
-                    state.credit += state.courseState[target.id - 1].credit
-                    state.focusState = target
-                }
-                
-            }else if(state.focusState.includes(state.courseState[target.id - 1].courseId)){
-                console.log(state.focusState)
-                state.credit -= state.courseState[target.id - 1].credit
-                state.focusState = target
-            }
-            
+            state.updateFocus(target)
+            //state.focusState = target;
         }
     }
-    const submit = () => {
+    const submitCourses = () => {
         const element = document.querySelector(view.selectorContainer.submit)
         element.addEventListener('click', eventHandler)
         function eventHandler(){
-            if(confirm(`You have chosen ${state.credit} credits for this semester. You cannot change once you submit. Do you want to confirm?`)){
-                state.submitState = state.courseState.filter((course)=>{
-                    return state.focusState.includes(course.courseId)
-                })
-                state.courseState = state.courseState.filter((course)=>{
-                    return !state.focusState.includes(course.courseId)
-                })
-
-            }
+            state.attemptSubmit([...state.focusState])
         }
     }
     const bootstrap = () => {
         init();
         focusCourse();
-        submit();
+        submitCourses();
     }
 	return {bootstrap}
 })(Model, View);
